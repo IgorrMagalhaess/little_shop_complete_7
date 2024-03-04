0def70f6d9900d8e26786d7f2967a76b55fc6465
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-03-04 10:57:12 -0500

    link to show page of discount within show page of invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -12,6 +12,15 @@ class Invoice < ApplicationRecord
   enum status: [:cancelled, :in_progress, :completed]
 
   def total_revenue
-    invoice_items.sum("unit_price * quantity")
+    invoice_items.sum("unit_price * quantity")/100
+  end
+
+  def total_revenue_with_discount
+    total_discount = invoice_items.joins(item: {merchant: :bulk_discounts})
+                                  .where("bulk_discounts.quantity <= invoice_items.quantity")
+                                  .where("bulk_discounts.merchant_id = merchants.id")
+                                  .maximum("bulk_discounts.percentage * invoice_items.unit_price * invoice_items.quantity / 100 / 100") #divide to 100 to get percentage and to get cents to dolar
+
+    total_revenue - (total_discount || 0)
   end
 end
